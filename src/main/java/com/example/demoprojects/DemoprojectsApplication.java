1424e8990ac4797a60f937ace9e2379f60d82d2e
liuruibing123 <2460852245lrb>
2021-02-07 15:40:22 +0800

    项目创建

---
+++ src/main/java/com/example/demoprojects/DemoprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoprojectsApplication.class, args);
+    }
+
+}
