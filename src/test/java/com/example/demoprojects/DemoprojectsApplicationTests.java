3c091895649a7bd2310e403e95c2aa810255eef2
liuruibing123 <2460852245lrb>
2021-02-07 16:29:59 +0800

    :new:修改编码

--- src/test/java/com/example/demoprojects/DemoprojectsApplicationTests.java
+++ src/test/java/com/example/demoprojects/DemoprojectsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demoprojects;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class DemoprojectsApplicationTests {
 
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void set(){
+        redisTemplate.opsForValue().set("myKey","myValue");
+        System.out.println(redisTemplate.opsForValue().get("myKey"));
     }
 
 }
