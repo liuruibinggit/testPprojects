6d76ae5bb069d4aa16d79277a9b0d77f9f8d1796
liuruibing123 <2460852245lrb>
2021-02-08 15:51:15 +0800

    :new:修改编码

--- src/main/java/com/example/demoprojects/controller/TestController.java
+++ src/main/java/com/example/demoprojects/controller/TestController.java
@@ -1,7 +1,15 @@
 package com.example.demoprojects.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @GetMapping(value = "/test")
+    public String test(){
+        System.out.println("test");
+        return "test";
+    }
+
 }
